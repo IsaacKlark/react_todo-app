639baf828b16a262fe4332cad1da03cddc94bb5a
IsaacKlark <isaackeklarke@gmail.com>
2019-12-12 20:16:18 +0200

    Updates

---
+++ precache-manifest.3c77f92eeb92766c0a995a5f9efa8638.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2874e555cb1f6cb3339efe85f17e111",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "60b2f920065fe82360b8",
+    "url": "/react_todo-app/static/css/main.62bbc3e1.chunk.css"
+  },
+  {
+    "revision": "0afbcebff7bcd2ae486d",
+    "url": "/react_todo-app/static/js/2.fc2620cb.chunk.js"
+  },
+  {
+    "revision": "60b2f920065fe82360b8",
+    "url": "/react_todo-app/static/js/main.181a7e75.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
